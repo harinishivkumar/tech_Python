656a79132e51ea14e220c47e114d8b82c94e79c6
Kashyap <tkashyap88@gmail.com>
2020-01-03 22:12:50 -0500

    initial commit

---
+++ api_basic.py
@@ -0,0 +1,14 @@
+import requests
+from pprint import pprint
+with open('city.txt','r') as C:
+  city = C.read()
+with open('key.txt','r') as K:
+  key = K.read()
+K.close()
+url = 'https://api.openweathermap.org/data/2.5/weather?q={}&appid={}'.format(city,key)
+res = requests.get(url)
+data = res.json()
+print(res)
+pprint (data)
+
+
